967acbc6d1345ef712590afe279a64d7a3213826
bravo9798 <bhuvneshsharma9798@gmail.com>
2023-12-07 15:05:02 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FlavanoidsStats from './components/Flavanoids';
+import GammaStats from './components/Gamma';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FlavanoidsStats/>  {/* Flavanoid table */}       
+        <GammaStats/>  {/* Gamma Table */}
       </header>
     </div>
   );
